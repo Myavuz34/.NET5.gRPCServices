fab942212380fb88ebc9280e02849bed5c0f2a7c
Myavuz34 <myavuz53@gmail.com>
2021-01-02 02:01:23 +0300

    * GrpcService.sln: * GrpcService.csproj: * GreeterService.cs:
    * weather.proto: updated naming

--- GrpcStockService/Services/GreeterService.cs
+++ GrpcStockService/Services/GreeterService.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
+using GrpcService;
 using Microsoft.Extensions.Logging;
 
 namespace GrpcStockService
